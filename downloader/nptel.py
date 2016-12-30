3c418f7463fe8af5dad57100083731413ab6b981
moghya <sawantshubham571@gmail.com>
2016-12-31 00:32:42 +0530

    working fine lecture gets downloaded

--- downloader/nptel.py
+++ downloader/nptel.py
@@ -9,7 +9,7 @@ def getCourseData(courseId):
     course = {
         "courseId" : str(courseId),
         "name": "",
-        "isFree" : False,
+        "isFree" : 0,
         "lecturesLinks":[],
     }
     s = r.session()
@@ -17,11 +17,11 @@ def getCourseData(courseId):
     if page.status_code!=200:
         return None
     else:
-        soup = b(page.text,'lxml')
+        soup = b(page.text,'html.parser')
         js = soup.find('script',type="application/ld+json").text
         jsobj = j.loads(js)
         if jsobj['isAccessibleForFree'] == "True":
-            course['isFree'] = True
+            course['isFree'] = 1
             course['name'] = jsobj['name']
             course['lecturesLinks'] = jsobj['partOfEducationalTrack']
     return course
@@ -38,7 +38,7 @@ def getLectureDownloadLink(link):
     if page.status_code!=200:
         return None
     else:
-        soup = b(page.text,'lxml')
+        soup = b(page.text,'html.parser')
         video = soup.find('div',{'id':'download'}).findAll('p')[0].findAll('a')[0].attrs['href']
         local_filename = video.split('/')[-1].split('.')[0] + '_' + soup.find('li',{'class':'here'}).text  +'.' + video.split('/')[-1].split('.')[1]
         lecture = {
--- downloader/views.py
+++ downloader/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 from django.core.urlresolvers import reverse
 from downloader.nptel import *
 import json
+import traceback
 
 def index(request):
     context = {
@@ -14,23 +15,26 @@ def index(request):
 
 def details(request):
     if  request.method == "POST":
-        try:
-            courseId = request.POST['courseId']
-            courseDetails = getCourseData(courseId)
-            context = {
-                'courseDetails' : courseDetails
-            }
-            return render(request,'downloader/details.html',context)
-        except:
-            return HttpResponse('Sorry we\'re down now :(')
-            pass
+        courseId = request.POST['courseId']
+        courseDetails = getCourseData(courseId)
+        context = {
+            'courseDetails' : courseDetails
+        }
+        return render(request,'downloader/details.html',context)
     else:
         return HttpResponse("you know i need course code for giving you details :)")
 
 def getLecture(request):
     if request.method == "POST":
-        link = request.POST['lectureLink']
-        lecture = getLectureDownloadLink(link)
-        return HttpResponse(json.dumps(lecture), content_type="application/json")
+        try:
+            link = request.POST['lectureLink']
+            lecture = getLectureDownloadLink(link)
+            return HttpResponse(json.dumps(lecture), content_type="application/json")
+        except:
+            lecture = {
+                'download':'',
+                'href':''
+            }
+            return HttpResponse(json.dumps(lecture), content_type="application/json")
     else:
         return HttpResponse("Sorry buddy nothing is here :)")
